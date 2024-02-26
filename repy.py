cb095ff35c8b68763cce7ddbb5cbfdae37d827e6
caolan947 <caolan.daly94@gmail.com>
2024-02-26 14:58:20 +0000

    created factory and classes with basic parsing and assigning attributes

--- repy.py
+++ repy.py
@@ -1,2 +1,41 @@
-def say_hello():
-    return "Hello world"
+
+
+import json
+
+# TODO unit test, integration test, add logging
+class EventFactory():
+    def __init__(self, event):
+        self.event = event
+
+    def get_event_parser(self):
+        if self.is_api_gw():
+            return UnpackEvent(self.event)
+        
+        elif self.is_sqs():
+            return UnpackSqsMessage(self.event)
+
+            
+    def is_api_gw(self):
+        try:
+            json.loads(self.event["body"])
+            return True
+        except (TypeError, KeyError):
+            return False
+
+    def is_sqs(self):
+        try:
+            json.loads(self.event["Records"][0]['body'])
+            return True
+        except Warning:
+            return False
+
+class UnpackEvent():
+    def __init__(self, event):
+        self.event = json.loads(event["body"])
+    
+class UnpackSqsMessage():
+    def __init__(self, event):
+        self.event = json.loads(event["Records"][0]['body'])
+
+
+
